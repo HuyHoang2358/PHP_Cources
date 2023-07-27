bc05331c98d49e8c19ae35a72f18431efed3a5c8
Tran Huy Hoang <hoangth33@cyberspace.vn>
2023-07-27 19:52:22 +0700

    module1 lesson2 code

---
+++ Module1_PHP_Basic/2_variables_operators_if_else/Codes/3_php_echo.php
@@ -0,0 +1,13 @@
+<?php
+echo "<h2>Text có gắn thẻ HTML</h2>";
+echo "Không xuống dòng nhé _(m_m)_ ";
+echo "Không xuống thì viết tiếp<br>";
+echo("Tôi là Robot <br>");
+
+
+$txt1 = "Echo ra biến";
+$x = 4;
+
+echo "<h2>" . $txt1 . "</h2>";
+echo "1 tháng thường có " . $x . " tuần <br>";
+
---
+++ Module1_PHP_Basic/2_variables_operators_if_else/Codes/4_php_print.php
@@ -0,0 +1,12 @@
+<?php
+print "<h2>Text có gắn thẻ HTML</h2>";
+print("Không xuống dòng nhé _(m_m)_ ");
+print "Không xuống thì viết tiếp<br>";
+print("Tôi là Robot <br>");
+
+
+$txt1 = "Echo ra biến";
+$x = 4;
+
+print "<h2>" . $txt1 . "</h2>";
+print "1 tháng thường có " . $x . " tuần <br>";
