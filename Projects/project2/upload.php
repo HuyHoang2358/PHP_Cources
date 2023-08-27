0db425ddb13d007c348926a954621ee5b56339e5
Huyhoang-OH-28 <huyhoang.tran6669@gmail.com>
2023-08-27 14:09:07 +0700

    project2

--- Projects/project2/upload.php
+++ Projects/project2/upload.php
@@ -2,52 +2,41 @@
 session_start();
 require_once __DIR__ . '/lib/flash.php';
 require_once __DIR__ . '/lib/functions.php';
+
+// 1. Cấu hình các định dạng file cho upload
 const ALLOWED_FILES = [
     'image/png' => 'png',
-    'image/jpeg' => 'jpg'
+    // viết thêm code
 ];
+// 2.Cấu hình dung lượng file lớn nhất được upload
 const MAX_SIZE = 5 * 1024 * 1024; //  5MB
+// 3. Cấu hình thư mục upload ảnh
 const UPLOAD_DIR = __DIR__ . '/uploads';
 
+// 4. Nhạn dữ liệu từ  form
 $is_post_request = strtolower($_SERVER['REQUEST_METHOD']) === 'post';
 $has_file = isset($_FILES['file']);
 
 
-if (!$is_post_request || !$has_file) {
-    redirect_with_message('Invalid file upload operation', FLASH_ERROR);
-}
+// 5. Kiểm tra file có tồn tại không, loại method
+// Viết code
 
-//
+// 6. Kiểm tra file
 $status = $_FILES['file']['error'];
 $filename = $_FILES['file']['name'];
 $tmp = $_FILES['file']['tmp_name'];
 
 
-// an error occurs
-if ($status !== UPLOAD_ERR_OK) {
-    redirect_with_message(get_message($status), FLASH_ERROR);
-}
+// 6.1 Kiểm tra file upload có thành công không
+// Viết code
 
-// validate the file size
-$filesize = filesize($tmp);
-if ($filesize > MAX_SIZE) {
-    redirect_with_message('Error! your file size is ' . format_filesize($filesize) . ' , which is bigger than allowed size ' . format_filesize(MAX_SIZE), FLASH_ERROR);
-}
+// 6.2 Kiểm tra dung lượng file
+// Viết code
 
-// validate the file type
-$mime_type = get_mime_type($tmp);
-if (!in_array($mime_type, array_keys(ALLOWED_FILES))) {
-    redirect_with_message('The file type is not allowed to upload', FLASH_ERROR);
-}
-// set the filename as the basename + extension
-$uploaded_file = pathinfo($filename, PATHINFO_FILENAME) . '.' . ALLOWED_FILES[$mime_type];
-// new file location
-$filepath = UPLOAD_DIR . '/' . $uploaded_file;
+// 6.3 Kiểm tra loại file upload
+// Viết code
 
-// move the file to the upload dir
-$success = move_uploaded_file($tmp, $filepath);
-if ($success) {
-    redirect_with_message('The file was uploaded successfully.', FLASH_SUCCESS);
-}
+// 7. Chuyển file từ folder tạm sang folder lưu chính
+// Viết code
 
-redirect_with_message('Error moving the file to the upload folder.', FLASH_ERROR);
+// 8. Hoàn thành điều hướng về index.php
